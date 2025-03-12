836fc0aeddd3f36ff1596a4347e77d2a35862f36
GeekCoder403 <124810233+Mpouel@users.noreply.github.com>
2025-03-12 11:32:39 -0400

    GeekCoder403 Update

--- random.js
+++ random.js
@@ -2,15 +2,19 @@ let last = 0;
 const Cooldown = 395;
 const iframe = document.querySelector("iframe")
 
-iframe.addEventListener('keydown', function(e) {
-    const now = new Date().getTime();
-    const time = Number(now - last);
-    
-    if ((time < Cooldown) && e.key === 'ArrowUp') {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        console.log(`Spam prevented: ${time}ms / ${Cooldown}ms`);
-    } else {
-        last = now;
-    }
-});
+iframe.onload = lol()
+
+function lol() {
+    iframe.addEventListener('keydown', function(e) {
+        const now = new Date().getTime();
+        const time = Number(now - last);
+        
+        if ((time < Cooldown) && e.key === 'ArrowUp') {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            console.log(`Spam prevented: ${time}ms / ${Cooldown}ms`);
+        } else {
+            last = now;
+        }
+    });
+}
